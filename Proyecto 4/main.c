e4f7280186ab2314b7fe2cf8c1e0d7497520f26e
Vivi <vivivar0110@gmail.com>
2025-10-19 16:53:07 -0600

    Entry Grids y Scrollables Listos

--- Proyecto 4/main.c
+++ Proyecto 4/main.c
@@ -31,17 +31,20 @@ GtkWidget *varQtyLabel;
 GtkWidget *spinVariables;
 GtkWidget *variablesScroll;
 GtkWidget *varnameLabel;
+GtkWidget *gridVariables; 
 
 GtkWidget *funcionLabel;
 GtkWidget *ZLabel;
-GtkWidget *variablesScroll1;
+GtkWidget *ZScroll;
+GtkWidget *ZGrid;
 
 GtkWidget *restQtyLabel;
 GtkWidget *maxminSelectionLabel;
 GtkWidget *sujetoLabel;
 GtkWidget *spinRestrictions;
 GtkWidget *restrictionsScroll;
-GtkWidget *restQtyLabel1;
+GtkWidget *xiLabel;
+GtkWidget *gridRestrictions;
 
 GtkWidget *continueButton;
 GtkWidget *loadFileButton;
@@ -51,9 +54,15 @@ GtkWidget *solveButton;
 GtkWidget *exitButton;
 
 GtkWidget *dantzigImage;
+GtkWidget *saveButton;
+GtkWidget *loadButton;
 
 GtkBuilder *builder;
 GtkCssProvider *cssProvider;
+
+// ---- Variables Globales ----
+
+const char *type = "MAX";
 
 
 /* 
@@ -71,12 +80,249 @@ void set_css (GtkCssProvider *cssProvider, GtkWidget *widget){
 	gtk_style_context_add_provider(styleContext,GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 }
 
+// -------------------------------------------
+// ----------------- Helpers -----------------
+// -------------------------------------------
 
+static void validateEntryNumber(GtkEditable *editable,const gchar *text, gint length, gint *position, gpointer user_data) {
+
+    const gchar *current = gtk_entry_get_text(GTK_ENTRY(editable));
+    gboolean isReal = (strchr(current, '.') != NULL);
+    gboolean isNegative = (current[0] == '-');
+
+    gint sel_start = 0, sel_end = 0;
+    gboolean has_sel = gtk_editable_get_selection_bounds(editable, &sel_start, &sel_end);
+
+    GString *filtered = g_string_sized_new(length);
+
+    for (int i = 0; i < length; i++) {
+        char c = text[i];
+
+        if (g_ascii_isdigit(c)) {
+            g_string_append_c(filtered, c);
+            continue;
+        }
+
+        if (c == '.') {
+            if (!isReal || (has_sel && strchr(current + sel_start, '.') < current + sel_end)) {
+                g_string_append_c(filtered, c);
+                isReal = TRUE;
+            }
+            continue;
+        }
+
+        if (c == '-') {
+            gboolean possibleNegative = FALSE;
+
+            int pos = *position + filtered->len;
+
+            if (!isNegative) {
+                if (pos == 0) {
+                    possibleNegative = TRUE;
+                } else if (has_sel && sel_start == 0) {
+                    possibleNegative = TRUE;
+                }
+            }
+
+            if (possibleNegative) {
+                g_string_append_c(filtered, c);
+                isNegative = TRUE;
+            }
+            continue;
+        }
+
+    }
+
+    if (filtered->len != length) {
+        g_signal_stop_emission_by_name(editable, "insert-text");
+        gtk_editable_insert_text(editable, filtered->str, filtered->len, position);
+    }
+    g_string_free(filtered, TRUE);
+}
+
+static gboolean normalizeValue(GtkWidget *entry, GdkEvent *event, gpointer user_data) {
+    const char *t = gtk_entry_get_text(GTK_ENTRY(entry));
+    if (t == NULL || *t == '\0' || (strcmp(t, "-") == 0) || (strcmp(t, ".") == 0) || (strcmp(t, "-.") == 0)) {
+        gtk_entry_set_text(GTK_ENTRY(entry), "0");
+    }
+    return FALSE; // seguir con el manejo normal del foco
+}
+
+
+// -------------------------------------------
+// ---------------- Funciones ----------------
+// -------------------------------------------
+
+static void clearContainer(GtkWidget *container) {
+    GList *children = gtk_container_get_children(GTK_CONTAINER(container));
+    for (GList *it = children; it != NULL; it = g_list_next(it)) {
+        gtk_widget_destroy(GTK_WIDGET(it->data));
+    }
+    g_list_free(children);
+}
+
+void createVariables(GtkSpinButton *spin, gpointer user_data) {
+    int cant = gtk_spin_button_get_value_as_int(spin);
+
+    GList *children = gtk_container_get_children(GTK_CONTAINER(gridVariables));
+    for (GList *i = children; i != NULL; i = g_list_next(i)) {
+        gtk_widget_destroy(GTK_WIDGET(i->data));
+    }
+    g_list_free(children);
+
+ 
+    for (int i = 0; i < cant; i++) {
+        gchar defaultName[16];
+        g_snprintf(defaultName, sizeof(defaultName), "X%d", i + 1);
+
+        GtkWidget *entry = gtk_entry_new();
+        gtk_entry_set_text(GTK_ENTRY(entry), defaultName);
+        gtk_entry_set_alignment(GTK_ENTRY(entry), 0.0); 
+
+        gtk_grid_attach(GTK_GRID(gridVariables), entry, 0, i, 1, 1);
+    }
+
+    gtk_widget_show_all(gridVariables);
+}
+
+static void createZ (void) {
+    int n = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinVariables));
+    if (n <= 0) {
+        clearContainer(ZGrid);
+        gtk_widget_show_all(ZGrid);
+        return;
+    }
+
+    clearContainer(ZGrid);
+
+    int col = 0; 
+
+    for (int i = 0; i < n; i++) {
+        GtkWidget *c = gtk_entry_new();
+        gtk_entry_set_width_chars(GTK_ENTRY(c), 6);    
+        gtk_entry_set_alignment(GTK_ENTRY(c), 1.0);
+        g_signal_connect(c, "insert-text", G_CALLBACK(validateEntryNumber), NULL);
+        g_signal_connect(c, "focus-out-event", G_CALLBACK(normalizeValue), NULL);    
+        gtk_grid_attach(GTK_GRID(ZGrid), c, col++, 0, 1, 1);
+
+        GtkWidget *nombreEntry = gtk_grid_get_child_at(GTK_GRID(gridVariables), 0, i);
+        const char *nombreVar = nombreEntry ? gtk_entry_get_text(GTK_ENTRY(nombreEntry)) : "X?";
+        GtkWidget *lblVar = gtk_label_new(nombreVar);
+        gtk_label_set_xalign(GTK_LABEL(lblVar), 0.0);
+        gtk_grid_attach(GTK_GRID(ZGrid), lblVar, col++, 0, 1, 1);
+
+        if (i < n - 1) {
+            GtkWidget *mas = gtk_label_new("+");
+            gtk_grid_attach(GTK_GRID(ZGrid), mas, col++, 0, 1, 1);
+        }
+    }
+
+    gtk_widget_show_all(ZGrid);
+}
+
+static void createRestrictions (void) {
+    int m = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinRestrictions));
+    int n = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinVariables));   
+
+    // Limpiar el grid de restricciones
+    clearContainer(gridRestrictions);
+
+    if (m <= 0 || n <= 0) {
+        gtk_widget_show_all(gridRestrictions);
+        return;
+    }
+
+    for (int r = 0; r < m; r++) {
+        int col = 0;
+
+        for (int i = 0; i < n; i++) {
+            GtkWidget *c = gtk_entry_new();
+            gtk_entry_set_width_chars(GTK_ENTRY(c), 6);
+            gtk_entry_set_alignment(GTK_ENTRY(c), 1.0);
+            gtk_entry_set_text(GTK_ENTRY(c), "0");
+            g_signal_connect(c, "insert-text", G_CALLBACK(validateEntryNumber), NULL);
+            g_signal_connect(c, "focus-out-event", G_CALLBACK(normalizeValue), NULL);
+            gtk_grid_attach(GTK_GRID(gridRestrictions), c, col++, r, 1, 1);
+
+            GtkWidget *nombreEntry = gtk_grid_get_child_at(GTK_GRID(gridVariables), 0, i);
+            const char *nombreVar = nombreEntry ? gtk_entry_get_text(GTK_ENTRY(nombreEntry)) : "X?";
+            GtkWidget *lblVar = gtk_label_new(nombreVar);
+            gtk_label_set_xalign(GTK_LABEL(lblVar), 0.0);
+            gtk_grid_attach(GTK_GRID(gridRestrictions), lblVar, col++, r, 1, 1);
+
+            if (i < n - 1) {
+                GtkWidget *mas = gtk_label_new("+");
+                gtk_grid_attach(GTK_GRID(gridRestrictions), mas, col++, r, 1, 1);
+            }
+        }
+
+
+        GtkWidget *rel = gtk_combo_box_text_new();
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rel), "≥");
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rel), "≤");
+        gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(rel), "=");
+        gtk_combo_box_set_active(GTK_COMBO_BOX(rel), 0); 
+        gtk_grid_attach(GTK_GRID(gridRestrictions), rel, col++, r, 1, 1);
+
+        GtkWidget *rhs = gtk_entry_new();
+        gtk_entry_set_width_chars(GTK_ENTRY(rhs), 8);
+        gtk_entry_set_alignment(GTK_ENTRY(rhs), 1.0);
+        gtk_entry_set_text(GTK_ENTRY(rhs), "0");
+        g_signal_connect(rhs, "insert-text", G_CALLBACK(validateEntryNumber), NULL);
+        g_signal_connect(rhs, "focus-out-event", G_CALLBACK(normalizeValue), NULL);
+        gtk_grid_attach(GTK_GRID(gridRestrictions), rhs, col++, r, 1, 1);
+    }
+
+    gtk_widget_show_all(gridRestrictions);
+}
+
+
+// -------------------------------------------
+// ----------------- BOTONES -----------------
+// -------------------------------------------
+
+void on_rbMaximizar_toggled(GtkRadioButton *rbMaximizar, gpointer user_data) {
+    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(rbMaximizar))) {
+        type = "MAX";
+    }
+}
+
+void on_rbMinimizar_toggled(GtkRadioButton *rbMinimizar, gpointer user_data) {
+    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(rbMinimizar))) {
+        type = "MIN";
+    }
+}
+
+void on_continueButton_clicked(GtkWidget *widget, gpointer data) {
+    createZ();
+    createRestrictions();
+    gtk_widget_set_sensitive(solveButton, TRUE);
+}
+
+void on_loadFileButton_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Cargar archivo presionado\n");
+}
+
+void on_solveButton_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Resolver presionado\n");
+}
+
+void on_saveButton_clicked(GtkWidget *widget, gpointer data) {
+    g_print("Guardar presionado\n");
+}
+
+void on_loadButton_clicked(GtkWidget *widget, gpointer data) {
+    gtk_widget_set_sensitive(solveButton, TRUE);
+}
 
 //Función de acción para el botón de 'Exit' que cierra todo el programa.
 void on_exitButton_clicked (GtkButton *exitButton, gpointer data){
 	gtk_main_quit();
 }
+
+
+
+
 
 
 //Main
@@ -102,16 +348,16 @@ int main (int argc, char *argv[]){
     varQtyLabel = GTK_WIDGET(gtk_builder_get_object(builder, "varQtyLabel"));
     spinVariables = GTK_WIDGET(gtk_builder_get_object(builder, "spinVariables"));
     variablesScroll = GTK_WIDGET(gtk_builder_get_object(builder, "variablesScroll"));
-    varnameLabel = GTK_WIDGET(gtk_builder_get_object(builder, "varQtyLabel1"));
+    varnameLabel = GTK_WIDGET(gtk_builder_get_object(builder, "varnameLabel"));
     funcionLabel = GTK_WIDGET(gtk_builder_get_object(builder, "funcionLabel"));
     ZLabel = GTK_WIDGET(gtk_builder_get_object(builder, "ZLabel"));
-    variablesScroll1 = GTK_WIDGET(gtk_builder_get_object(builder, "variablesScroll1"));
+    ZScroll = GTK_WIDGET(gtk_builder_get_object(builder, "ZScroll"));
     restQtyLabel = GTK_WIDGET(gtk_builder_get_object(builder, "restQtyLabel"));
     maxminSelectionLabel = GTK_WIDGET(gtk_builder_get_object(builder, "maxminSelectionLabel"));
     sujetoLabel = GTK_WIDGET(gtk_builder_get_object(builder, "sujetoLabel"));
     spinRestrictions = GTK_WIDGET(gtk_builder_get_object(builder, "spinRestrictions"));
     restrictionsScroll = GTK_WIDGET(gtk_builder_get_object(builder, "restrictionsScroll"));
-    restQtyLabel1 = GTK_WIDGET(gtk_builder_get_object(builder, "restQtyLabel1"));
+    xiLabel = GTK_WIDGET(gtk_builder_get_object(builder, "xiLabel"));
     continueButton = GTK_WIDGET(gtk_builder_get_object(builder, "continueButton"));
     loadFileButton = GTK_WIDGET(gtk_builder_get_object(builder, "loadFileButton"));
     cargarLabel = GTK_WIDGET(gtk_builder_get_object(builder, "cargarLabel"));
@@ -119,7 +365,8 @@ int main (int argc, char *argv[]){
     solveButton = GTK_WIDGET(gtk_builder_get_object(builder, "solveButton"));
     exitButton = GTK_WIDGET(gtk_builder_get_object(builder, "exitButton"));
     dantzigImage = GTK_WIDGET(gtk_builder_get_object(builder, "dantzigImage"));
-
+    saveButton = GTK_WIDGET(gtk_builder_get_object(builder, "saveButton"));
+    loadButton = GTK_WIDGET(gtk_builder_get_object(builder, "loadButton"));
 
 	cssProvider = gtk_css_provider_new();
 	gtk_css_provider_load_from_path(cssProvider, "theme.css", NULL);
@@ -130,9 +377,37 @@ int main (int argc, char *argv[]){
 	set_css(cssProvider, solveButton);
 	set_css(cssProvider, exitButton);
 
-	g_signal_connect(exitButton, "clicked", G_CALLBACK(on_exitButton_clicked), NULL);
+    gtk_widget_set_sensitive(solveButton, FALSE);
 
-	
+	g_signal_connect(exitButton, "clicked", G_CALLBACK(on_exitButton_clicked), NULL);
+    g_signal_connect(continueButton, "clicked", G_CALLBACK(on_continueButton_clicked), NULL);
+    g_signal_connect(loadFileButton, "clicked", G_CALLBACK(on_loadFileButton_clicked), NULL);
+    g_signal_connect(solveButton, "clicked", G_CALLBACK(on_solveButton_clicked), NULL);
+    g_signal_connect(exitButton, "clicked", G_CALLBACK(on_exitButton_clicked), NULL);
+    g_signal_connect(saveButton, "clicked", G_CALLBACK(on_saveButton_clicked), NULL);
+    g_signal_connect(loadButton, "clicked", G_CALLBACK(on_loadButton_clicked), NULL);
+
+    gridVariables = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID(gridVariables), 6);
+    gtk_grid_set_column_spacing(GTK_GRID(gridVariables), 6);
+
+    ZGrid = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(ZGrid), 8);
+    gtk_grid_set_row_spacing(GTK_GRID(ZGrid), 6);
+    gtk_container_add(GTK_CONTAINER(ZScroll), ZGrid);
+
+    gridRestrictions = gtk_grid_new();
+    gtk_grid_set_column_spacing(GTK_GRID(gridRestrictions), 8);
+    gtk_grid_set_row_spacing(GTK_GRID(gridRestrictions), 6);
+    gtk_container_add(GTK_CONTAINER(restrictionsScroll), gridRestrictions);
+
+    gtk_container_add(GTK_CONTAINER(variablesScroll), gridVariables);
+        
+    g_signal_connect(spinVariables, "value-changed",G_CALLBACK(createVariables), NULL);
+    
+    g_signal_connect(rbMaximizar, "toggled", G_CALLBACK(on_rbMaximizar_toggled), NULL);
+    g_signal_connect(rbMinimizar, "toggled", G_CALLBACK(on_rbMinimizar_toggled), NULL);
+
 	gtk_widget_show(window);
 	
 	gtk_main();
